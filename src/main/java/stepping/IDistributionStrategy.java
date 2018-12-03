2a24855ff54a0c9406cbdbbda358d6f0f05e2352
gabi.beyo <gabi.beyo@imperva.com>
2018-12-03 15:41:24 +0200

    add todo

--- src/main/java/stepping/IDistributionStrategy.java
+++ src/main/java/stepping/IDistributionStrategy.java
@@ -3,5 +3,6 @@ package stepping;
 import java.util.List;
 
 public interface IDistributionStrategy {
+    //todo StepDecorator should not be exposed to API consumers
     void distribute(List<IStepDecorator> steps, Data data, String subjectType);
 }
