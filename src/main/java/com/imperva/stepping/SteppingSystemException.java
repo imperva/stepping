2ae6600448323a8917d51b9d7fc63ac10acf44de
gabi.beyo <gabi.beyo@imperva.com>
2019-04-23 22:13:36 +0300

    trivial support of String error

--- src/main/java/com/imperva/stepping/SteppingSystemException.java
+++ src/main/java/com/imperva/stepping/SteppingSystemException.java
@@ -2,6 +2,9 @@ package com.imperva.stepping;
 
 public class SteppingSystemException extends RuntimeException {
 
+    SteppingSystemException(String message) {
+        super(message);
+    }
 
     SteppingSystemException(Exception e) {
         super(e);
