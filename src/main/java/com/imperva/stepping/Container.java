0b3d2303f2ce0c25ff99a54982538b17755acd67
gabi.beyo <gabi.beyo@imperva.com>
2019-10-06 13:42:05 +0300

    fix container

--- src/main/java/com/imperva/stepping/Container.java
+++ src/main/java/com/imperva/stepping/Container.java
@@ -1,110 +1,30 @@
 package com.imperva.stepping;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
-public class Container {
-    private volatile ConcurrentHashMap<String,Identifiable> objects = new ConcurrentHashMap<>();
+public interface Container {
 
-    public Container() {
-    }
+    <T> ContainerDefaultImpl add(T obj);
 
-    public <T> Container add(T obj) {
-        add(new Identifiable<T>(obj, obj.getClass().getName() + obj.hashCode()));
-        return this;
-    }
+    <T> ContainerDefaultImpl add(T obj, String id);
 
-    public <T> Container add(T obj, String id) {
-        add(new Identifiable<T>(obj, id));
-        return this;
-    }
+    <T> ContainerDefaultImpl add(Identifiable<T> identifiable);
 
-    public <T> Container add(Identifiable<T> identifiable) {
-        if (objects.containsKey(identifiable.getId()))
-            throw new IdentifiableSteppingException(identifiable.getId(), "Identifiable Object must contain unique ID. " + identifiable.getId() + " already exists!");
-        objects.putIfAbsent(identifiable.getId(),identifiable);
-        return this;
-    }
+    ContainerDefaultImpl remove(String id);
 
-    public Container remove(String id) {
-        objects.remove(id);
-        return this;
-    }
+    ContainerDefaultImpl add(List<Identifiable> identifiables);
 
-    public Container add(List<Identifiable> identifiables) {
-        if(identifiables == null)
-            throw new SteppingException("Can't add null object to Container");
-        for (Identifiable identifiable :  identifiables) {
-            add(identifiable);
-        }
-        return this;
-    }
+    <T> T getById(String id);
 
-    public <T> T getById(String id) {
-        Identifiable obj = objects.get(id);
-        if(obj == null)
-            return null;
 
-        return (T)obj.get();
-    }
+    <T> List<T> getSonOf(Class<?> interf);
 
-    private boolean getSonOf(Class<?> interf, Class clss, List<Class> context) {
-        if (clss == null) {
-            for (Class c : context) {
-                if (c.equals(interf)) {
-                    return true;
-                }
-            }
-            return false;
-        }
+    <T> List<T> getTypeOf(Class<?> interf);
 
-        if (clss.equals(interf)) {
-            return true;
-        }
+    void clear();
 
-        for (Class intfc : clss.getInterfaces()) {
-            if (intfc.equals(interf)) {
-                return true;
-            }
-            context.addAll(Arrays.asList(intfc.getInterfaces()));
-        }
-        return getSonOf(interf, clss.getSuperclass(), context);
-    }
+    int size();
 
-    public <T> List<T> getSonOf(Class<?> interf) {
-        List<T> ts = new ArrayList<>();
-        List<Object> objects2 = objects.entrySet().stream().map((obj) -> (obj.getValue()).get()).collect(Collectors.toList());
-        for (Object o : objects2) {
-            Boolean found = getSonOf(interf, o.getClass(), new ArrayList<>());
-            if (found)
-                ts.add((T) o);
-        }
-        return ts;
-    }
-
-    public <T> List<T> getTypeOf(Class<?> interf) {
-        List<T> ts = new ArrayList<>();
-        List<Object> objects2 = objects.entrySet().stream().map((obj) -> (obj.getValue()).get()).collect(Collectors.toList());
-        for (Object o : objects2) {
-            if (o.getClass().equals(interf))
-                ts.add((T) o);
-        }
-        return ts;
-    }
-
-    public void clear() {
-        if (objects != null)
-            objects.clear();
-    }
-
-    public int size() {
-        return objects.size();
-    }
-
-    public boolean exist(String name) {
-       return objects.containsKey(name);
-    }
+    boolean exist(String name);
 }
+
