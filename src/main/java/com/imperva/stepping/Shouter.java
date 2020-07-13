0bae62332cc93df8efe8ec3fb856bf911c914c1f
gabi.beyo <gabi.beyo@imperva.com>
2020-07-13 12:11:20 +0300

    not fail when shouting an orphan subject

--- src/main/java/com/imperva/stepping/Shouter.java
+++ src/main/java/com/imperva/stepping/Shouter.java
@@ -15,7 +15,9 @@ import org.slf4j.LoggerFactory;
 
      public void shout(String subjectType, Object value) {
          try {
-             container.<Subject>getById(subjectType).publish(value);
+             Subject subjectToNotify = handleMissingSubject(subjectType);
+             if (subjectToNotify == null) return;
+             subjectToNotify.publish(value);
          } catch (Exception e) {
              logger.error("Shouter Failed", e);
              rootExceptionHandler.handle(new SteppingDistributionException(subjectType, "Distribution FAILED", e));
@@ -24,10 +26,21 @@ import org.slf4j.LoggerFactory;
 
      public void shout(String subjectType, Data value) {
          try {
-             container.<Subject>getById(subjectType).publish(value);
+             Subject subjectToNotify = handleMissingSubject(subjectType);
+             if (subjectToNotify == null) return;
+             subjectToNotify.publish(value);
          } catch (Exception e) {
              logger.error("Shouter Failed", e);
              rootExceptionHandler.handle(new SteppingDistributionException(subjectType, "Distribution FAILED", e));
          }
+     }
+
+     private Subject handleMissingSubject(String subjectType) {
+         Subject subjectToNotify = container.getById(subjectType);
+         if (subjectToNotify == null) {
+             logger.error("Subject '" + subjectType + "' is missing, can't shout! Make sure that there is at least 1 follower attached to it");
+             return null;
+         }
+         return subjectToNotify;
      }
  }
