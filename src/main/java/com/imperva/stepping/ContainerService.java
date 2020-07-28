eb25f346e3a5afa7ebb53d1ba7b5c2a0e7ee8647
linda.nasredin <linda.nasredin@imperva.com>
2020-07-28 16:37:59 +0300

    Added first unit tests

--- src/main/java/com/imperva/stepping/ContainerService.java
+++ src/main/java/com/imperva/stepping/ContainerService.java
@@ -2,10 +2,14 @@ package com.imperva.stepping;
 
 public class ContainerService extends ContainerDefaultImpl {
 
+    static final String RUNNING_SCHEDULED = ".runningScheduled";
+    static final String DECORATOR = ".decorator";
+    static final String STEPPING_PRIVATE_CONTAINER = "__STEPPING_PRIVATE_CONTAINER__";
+
     public RunningScheduled getTickCallbackRunning(String stepID) {
         try {
-            String runningID = stepID + ".runningScheduled";
-            return ((RunningScheduled) ((Container) getById("__STEPPING_PRIVATE_CONTAINER__")).getById(runningID));
+            String runningID = stepID + RUNNING_SCHEDULED;
+            return ((RunningScheduled) ((Container) getById(STEPPING_PRIVATE_CONTAINER)).getById(runningID));
         } catch (Exception e) {
             throw new SteppingException("Running object not found. Please make sure that you enabled TickCallback for this step");
         }
@@ -13,8 +17,8 @@ public class ContainerService extends ContainerDefaultImpl {
 
     public int getQSize(String stepID) {
         try {
-            String decoratorID = stepID + ".decorator";
-            return ((IStepDecorator) ((Container) getById("__STEPPING_PRIVATE_CONTAINER__")).getById(decoratorID)).getQSize();
+            String decoratorID = stepID + DECORATOR;
+            return ((IStepDecorator) ((Container) getById(STEPPING_PRIVATE_CONTAINER)).getById(decoratorID)).getQSize();
         } catch (Exception e) {
             throw new SteppingException("Q size not found. Please make sure you use the correct Step ID");
         }
